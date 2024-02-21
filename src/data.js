a6c431cc8eb29191a986f25cf5b03d2fca4716be
Khin Myat Mon <58884581+KMyatMon@users.noreply.github.com>
2024-02-21 14:20:54 +0630

    src folder commit

---
+++ src/data.js
@@ -0,0 +1,41 @@
+// projectsData.js
+import ibgroup from '/src/assets/images/odin-statch.png';
+import memento from '/src/assets/images/urdan.png';
+import acc from '/src/assets/images/orebi.png';
+import daddy from '/src/assets/images/godaddy-desktop.webp';
+import sunnyside from '/src/assets/images/sunny-side-square.webp';
+
+const projectsData = [
+  {
+    title: 'Odin Etch Sketch Website',
+    description: 'Web Design / Frontend Development',
+    imageSrc: ibgroup,
+    link: 'https://github.com/KMyatMon/Odin_Etch-a-Sketch'
+  },
+  {
+    title: 'Furniture Store',
+    description: 'Web Design / Frontend Development',
+    imageSrc: memento,
+    link: 'https://kmyatmon.github.io/furniture_store/'
+  },
+  {
+    title: 'Orebi Fashion House Clone',
+    description: 'Web Design / Frontend Development',
+    imageSrc: acc,
+    link: 'https://realbusinessaccountants.netlify.app'
+  },
+  {
+    title: 'GoDaddy Landing Page Clone',
+    description: 'Web Design / Frontend Development',
+    imageSrc: daddy,
+    link: 'https://godaddyuiclone.netlify.app'
+  },
+  {
+    title: 'Sunnyside Landing Page',
+    description: 'Web Design / Frontend Development',
+    imageSrc: sunnyside,
+    link: 'https://sunnysidechallenge.netlify.app'
+  }
+];
+
+export default projectsData;
